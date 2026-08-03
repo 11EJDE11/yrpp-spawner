a496f7ea0417c72f74f0ff9cb215c0ba83480ec1
11EJDE11 <jacobseliot54@gmail.com>
2026-08-03 22:25:11 +1200

    Remove build scripts

--- scripts/build_qm_release_cncnet_yr.bat
+++
@@ -1,8 +0,0 @@
-@if not defined _echo echo off
-
-rem Builds YRpp-Spawner Release-CnCNetYR as the QM replay beta variant (CnCNet-QM-Spawner.dll).
-
-rem Ensure we're in correct directory.
-cd /D "%~dp0"
-
-call build_qm Release-CnCNetYR
